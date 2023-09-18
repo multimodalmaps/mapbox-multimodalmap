1695159fd95b293b1fe09619252c8f9ed1dbacbd
hanselguzman-soto <soto26938@gmail.com>
2023-09-17 23:46:44 -0700

    add pointer gesture

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ import PinchZoomHandler from "./PinchZoomHandler";
 // eslint-disable-next-line no-unused-vars
 import VoiceRecognition from "./VoiceRecognition"; // <-- Import the new component
 import LandmarkOverlay from "./LandmarkOverlay"; // Import at the top
+import FingerPointHandler from "./FingerPointerHandler";
 
 function App() {
+  const pointerMoveSensitivity = 10;
+
   const [viewport, setViewport] = useState({
     latitude: 37.7577,
     longitude: -122.4376,
@@ -20,6 +23,14 @@ function App() {
     setViewport((prevViewport) => ({
       ...prevViewport,
       zoom: Math.min(Math.max(prevViewport.zoom - pinchDelta * 50, 0), 22),
+    }));
+  };
+
+  const onPointMove = (movement) => {
+    setViewport((prevViewport) => ({
+      ...prevViewport,
+      latitude: prevViewport.latitude - movement.dy * pointerMoveSensitivity,
+      longitude: prevViewport.longitude - movement.dx * pointerMoveSensitivity,
     }));
   };
 
@@ -37,7 +48,9 @@ function App() {
       <MapComponent viewport={viewport} onViewportChange={setViewport} />
       <VideoCanvas onLandmarks={setLandmarks} />
       <LandmarkOverlay landmarks={landmarks} />
-      <PinchZoomHandler onZoom={handleZoom} landmarks={landmarks} />
+      {/* <PinchZoomHandler onZoom={handleZoom} landmarks={landmarks} /> */}
+      <FingerPointHandler onPointMove={onPointMove} landmarks={landmarks} />
+
       {/* <VoiceRecognition onLocationUpdate={handleLocationUpdate} /> */}
     </div>
   );
--- src/PinchZoomHandler.js
+++ src/PinchZoomHandler.js
@@ -23,19 +23,33 @@ export default function PinchZoomHandler({ onZoom, landmarks }) {
       const indexTip = hand[8];
       const debouncedOnZoom = debounce(onZoom, PINCH_ZOOM_TIMEOUT_MILISECONDS);
 
-      const pinchDistance = Math.abs(thumbTip.y - indexTip.y);
-      console.log("pinchDistance:", pinchDistance);
-      if (lastPinchDistance !== null) {
-        const pinchDelta = pinchDistance - lastPinchDistance;
+      if (isValidPinchGesture(thumbTip, indexTip)) {
+        const pinchDistance = Math.abs(thumbTip.y - indexTip.y);
+        console.log("pinchDistance:", pinchDistance);
+        if (lastPinchDistance !== null) {
+          const pinchDelta = pinchDistance - lastPinchDistance;
 
-        // Update zoom based on pinch
-        if (Math.abs(pinchDelta) > PINCH_THRESHOLD) {
-          debouncedOnZoom(pinchDelta);
+          // Update zoom based on pinch
+          if (Math.abs(pinchDelta) > PINCH_THRESHOLD) {
+            debouncedOnZoom(pinchDelta);
+          }
         }
-      }
 
-      setLastPinchDistance(pinchDistance);
+        setLastPinchDistance(pinchDistance);
+      }
     }
+  };
+
+  const isValidPinchGesture = (thumbTip, indexTip) => {
+    // Calculate angle
+    let angle =
+      Math.atan2(indexTip.y - thumbTip.y, indexTip.x - thumbTip.x) *
+      (180 / Math.PI);
+    console.log("angle:", angle);
+    angle = Math.abs(angle);
+
+    // Check if the angle is approximately towards each other
+    return angle > 105 && angle < 90;
   };
 
   function debounce(func, wait) {
